a3d015c3342337f40e444679aeb9097292407b6c
ZubairAhmed <zubairahmedkhan619@gmail.com>
2025-07-23 16:57:07 +0400

    new

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
    output: 'export',
-  /* config options here */
-  // ignoreDuringBuilds: true,
-    eslint: {
-    ignoreDuringBuilds: true, // disables ESLint checks during next build
-  },
-
+   images: {
+     unoptimized: true,
+   },
+   // ignoreDuringBuilds: true,
+   eslint: {
+     ignoreDuringBuilds: true, // disables ESLint checks during next build
+   },
 };
 
 export default nextConfig;
